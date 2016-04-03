952611094402f57e8b7c631368ef561b95e0d156
daver234 <dave.rothschild@gmail.com>
2016-04-03 15:29:54 -0700

    updates to AutoLayout to get all boxes consistent on rotation

--- StrutsProblem/ViewController.swift
+++ StrutsProblem/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topLeftView: UIView!
+    @IBOutlet weak var topRightView: UIView!
+    @IBOutlet weak var bottomView: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
 
